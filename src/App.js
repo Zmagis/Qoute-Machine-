ac667916b79e18cb78aec28ba067b3b3c29d4958
Egle Gustaite <eglegustaite@gmail.com>
2019-11-25 11:01:02 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import QouteBox from "./Qoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>QOUTE MACHINE</h1>
+      <QouteBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Qoutes.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import "./Qoutes.css";
+import twitterLogo from "./blue.png";
+
+function QouteBox() {
+  let randomQoute;
+  const [qoute, setQoute] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    getQoute();
+  }, []);
+
+  const getQoute = async () => {
+    const response = await fetch(
+      `https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json`
+    );
+    const data = await response.json();
+
+    randomQoute = data[Math.floor(Math.random() * data.length)];
+    setAuthor(randomQoute.author);
+    setQoute(randomQoute.quote);
+  };
+
+  const nextQoute = e => {
+    e.preventDefault();
+    getQoute();
+    console.log("hi");
+  };
+
+  // const handleShake = () => {
+  //   const element = document.querySelector(".button");
+  //   element.style.animation = "shake 2s ease";
+  // };
+
+  return (
+    <div id="qoute-box">
+      <div id="text">
+        <p id="qoute">
+          <span
+            id="span"
+            style={{ position: "relative", top: "-0.5em", fontSize: "100%" }}
+          >
+            ,,{" "}
+          </span>
+          {qoute}
+          <span
+            id="span"
+            style={{ position: "relative", top: "-0.5em", fontSize: "100%" }}
+          >
+            {"  "}
+            ,,
+          </span>
+        </p>
+        <p id="author">- {author}</p>
+      </div>
+
+      <a id="tweet-qoute" href="https://twitter.com/" target="blank">
+        <img src={twitterLogo} alt="twitter-logo" />
+      </a>
+      <button
+        id="new-qoute"
+        className="button jump"
+        onClick={nextQoute}
+        // onMouseOver={handleJump}
+      >
+        Next qoute
+      </button>
+    </div>
+  );
+}
+
+export default QouteBox;
